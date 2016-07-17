e366ae2dd08cc045907269b3fe655dc2d5ff69b7
Jacob Zelek <jacob.zelek@gmail.com>
2016-07-17 00:46:50 -0700

    Additional config

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from flask import Flask, request
+import json
+import socket
+
 app = Flask(__name__)
 
-API_KEY = "1234567890"
+API_KEYS = {}
+
+CARBON_SOCKET = None
+
+
+def connect_to_carbon():
+    CARBON_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    CARBON_SOCKET.connect(("127.0.0.1", 2003))
+
+
+def close_carbon():
+    CARBON_SOCKET.close()
+
 
 @app.route('/metrics', methods=["POST"])
 def post_metric():
-    if request.headers.get("x-auth-token", None) == API_KEY:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("127.0.0.1", 2003))
-        s.send(request.body)
-        s.close()
+    if request.args.get("api_key", None) in API_KEYS:
+        try:
+            CARBON_SOCKET.send(request.body)
+        except:
+            connect_to_carbon()
+            CARBON_SOCKET.send(request.body)
+        
+        return "OK", 200
 
-    return "OK", 200
+    return "Unauthorized", 401
 
 if __name__ == "__main__":
-	app.run(debug=False, use_reloader=False, host="127.0.0.1", port=8081, threaded=True)
+    API_KEYS = json.load(open("config.json", "r"))["api_keys"]
+
+    connect_to_carbon()
+
+    app.run(debug=True, use_reloader=False, host="127.0.0.1", port=8081, threaded=True)
+
+    close_carbon()
