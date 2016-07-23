873e8f5216d45cc2f3d309ddafae163018f26e04
Jacob Zelek <jacob.zelek@gmail.com>
2016-07-23 14:17:44 -0700

    Updates to config options and nginx

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from functools import wraps
 
 app = Flask(__name__)
 
-API_KEYS = {}
+CONFIG = json.load(open("config.json", "r"))
+API_KEYS = CONFIG["api_keys"]
 
 
 def requires_auth_key(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
-        if request.args.get("api_key", None) not in API_KEYS:
+        api_key = request.args.get("api_key", None)
+        if api_key not in API_KEYS:
             return "Unauthorized", 401
+        else:
+            if not API_KEYS[api_key]["enabled"]:
+                return "Unauthorized", 401
         return func(*args, **kwargs)
     return wrapped
 
@@ -22,7 +27,7 @@ def requires_auth_key(func):
 @requires_auth_key
 def post_metric():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", 2003))
+    s.connect((CONFIG["carbon"]["host"], int(CONFIG["carbon"]["port"])))
     s.send("%s\n" % request.data)
     s.close()
     
@@ -32,12 +37,11 @@ def post_metric():
 @app.route('/carbon/events', methods=["POST"])
 @requires_auth_key
 def post_event():
-    req = urllib2.Request('http://127.0.0.1:8080/events', data=request.data, headers={'Content-type': 'application/json'})
+    req = urllib2.Request('http://{host}:{port}/events'.format(**CONFIG["graphite"]),
+        data=request.data, headers={'Content-type': 'application/json'})
     urllib2.urlopen(req)
     
     return "OK", 200
 
 if __name__ == "__main__":
-    API_KEYS = json.load(open("config.json", "r"))["api_keys"]
-
     app.run(debug=False, use_reloader=False, host="127.0.0.1", port=8081, threaded=True)
